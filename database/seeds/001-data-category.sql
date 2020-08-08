861c7c5655370c6c05293d24bdf6f31296355d13
orlov <yuraigle@gmail.com>
2020-08-08 12:08:43 +0800

    +faked cats&users

--- database/seeds/001-data-category.sql
+++ database/seeds/001-data-category.sql
@@ -1,8 +1,6 @@
 SET NAMES 'utf8';
 SET SESSION foreign_key_checks=OFF;
 
-DELETE FROM `adz_category`;
-
 INSERT INTO `adz_category` (`id`, `name`, `slug`, `description`, `keywords`, `parent_id`) VALUES
 (1001, 'Cars & Vehicles', 'cars', NULL, NULL, NULL),
 (1002, 'For Sale', 'for-sale', NULL, NULL, NULL),
@@ -10,11 +8,6 @@ INSERT INTO `adz_category` (`id`, `name`, `slug`, `description`, `keywords`, `pa
 (1004, 'Jobs', 'jobs', NULL, NULL, NULL),
 (1005, 'Services', 'business-services', NULL, NULL, NULL),
 (1006, 'Community', 'community', NULL, NULL, NULL),
-(1007, 'Pets', 'pets', NULL, NULL, NULL),
-(2485, 'Appliances', 'kitchen-appliances', NULL, NULL, 1002),
-(2488, 'Video Games & Consoles', 'video-games-consoles', NULL, NULL, 1002),
-(2495, 'Musical Instruments & DJ Equipment', 'music-instruments', NULL, NULL, 1002),
-(2496, 'Clothes, Footwear & Accessories', 'clothing', NULL, NULL, 1002),
-(2497, 'Baby & Kids Stuff', 'baby-kids-stuff', NULL, NULL, 1002);
+(1007, 'Pets', 'pets', NULL, NULL, NULL);
 
 SET SESSION foreign_key_checks=ON;
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -6,6 +6,14 @@ use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
+    /* @var \Faker\Generator */
+    private static $faker;
+
+    public function __construct()
+    {
+        self::$faker = Faker\Factory::create();
+    }
+
     /**
      * Run the database seeds.
      *
@@ -17,13 +25,60 @@ class DatabaseSeeder extends Seeder
         $this->command->info('DB created');
 
         DB::unprepared(file_get_contents('database/seeds/001-data-category.sql'));
-        $this->command->info('Categories imported');
+        $this->fakeCategories();
+        $this->command->info('Fake categories created');
 
-        DB::insert("insert into `adz_user` (`email`, `password`, `created_at`, `role`, `name`) values (?,?,now(),?,?)",
-            ['orlov@adz.me', Hash::make('asdasd'), 'admin', 'Yuri Orlov']);
-        DB::insert("insert into `adz_user` (`email`, `password`, `created_at`, `role`, `name`) values (?,?,now(),?,?)",
-            ['member@adz.me', Hash::make('qweqwe'), 'member', 'John Doe']);
-        $this->command->info('Users imported');
+        DB::insert("insert into `adz_user` (`email`, `password`, `created_at`, `role`, `name`)
+                    values (?,?,now(),?,?)", ['orlov@adz.me', Hash::make('asdasd'), 'admin', 'Yuri Orlov']);
+        $this->fakeUsers();
+        $this->command->info('Fake users created');
+
+    }
+
+    private function makeSlug($name) {
+        $slug = preg_replace('/[^a-z0-9]/', '-', strtolower($name));
+        $slug = preg_replace('/-+/', '-', $slug);
+        $slug = preg_replace('/-$/', '', $slug);
+        if (preg_match('/[^-]{3}/', $slug)) {
+            $slug = preg_replace('/-[^-]{1,2}-/', '-', $slug);
+            $slug = preg_replace('/^[^-]{1,2}-/', '', $slug);
+            $slug = preg_replace('/-[^-]{1,2}$/', '', $slug);
+        }
+
+        return $slug;
+    }
+
+    private function fakeCategories()
+    {
+        $roots = DB::select("select id from `adz_category` where `parent_id` is null");
+        foreach ($roots as $root) {
+            $parentId = $root->id;
+            $childrenCnt = self::$faker->numberBetween(2, 15);
+
+            for ($i = 0; $i < $childrenCnt; $i++) {
+                $name = self::$faker->unique()->sentence(4);
+                $slug = $this->makeSlug($name);
+                $description = self::$faker->text(250);
+                $keywords = join(',', self::$faker->words(7));
+
+                DB::insert("insert into `adz_category` (`name`, `slug`, `description`, `keywords`, `parent_id`)
+                    values (?,?,?,?,?)", [$name, $slug, $description, $keywords, $parentId]);
+            }
+        }
+    }
+
+    private function fakeUsers()
+    {
+        $pass1 = Hash::make('qweqwe');
+
+        for ($i = 0; $i < 1000; $i++) {
+            $email = self::$faker->unique()->email;
+            $name = self::$faker->name;
+            $created = self::$faker->dateTimeThisYear;
+
+            DB::insert("insert into `adz_user` (`email`, `password`, `created_at`, `role`, `name`)
+                        values (?,?,?,?,?)", [$email, $pass1, $created, 'member', $name]);
+        }
     }
 
 }
